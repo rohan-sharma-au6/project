2f4fd254924193150f287cc8744ee58c15f6ae94
rohan sharma <realrohan09@gmail.com>
2020-04-08 22:59:10 +0530

    abcd

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const app = require('./app');
 
 const port= process.env.PORT||3000
+
+app.get("/",function(req,res){
+    res.send("hello")
+})
 app.listen(port, function (req,res) {
     console.log("Server started on",port);
 });
